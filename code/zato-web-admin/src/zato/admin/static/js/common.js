c19f9635c93cf0ac9bef6abb34a079f0858dc366
Dariusz Suchojad <dsuch-github@m.zato.io>
2019-05-19 17:42:36 +0200

    GH #958 - Added is_tls_enabled to AMQP definitions.

--- code/zato-web-admin/src/zato/admin/static/js/common.js
+++ code/zato-web-admin/src/zato/admin/static/js/common.js
@@ -900,7 +900,8 @@ $.fn.zato.get_random_string = function() {
 
 $.fn.zato.to_bool = function(item) {
     var s = new String(item).toLowerCase();
-    return(s == "true" || s == 'on'); // 'on' too because it may be a form's field
+    result = (s === "true" || s === "on"); // 'on' too because it may be a form's field
+    return result;
 }
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
